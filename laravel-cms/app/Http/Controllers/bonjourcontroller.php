8aeff481e5433c1f4074507facd5963e285ecbe8
Bathily Yahaya <developpeur.yb@gmail.com>
2024-01-23 14:21:30 +0100

    premiers tests

---
+++ laravel-cms/app/Http/Controllers/bonjourcontroller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class bonjourcontroller extends Controller{
+    public function afficheBonjour(){
+        return "terst";
+    }
+}
